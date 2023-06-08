6b9243487a85c235bca1f91bc1bf0655f273ffd7
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-08 12:04:04 -0300

    add create method implementation

--- src/holders/holders.service.ts
+++ src/holders/holders.service.ts
@@ -1,10 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { ICreateHolderData, IHoldersService } from './holders.interfaces';
 import { Holder } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+import holdersErrors from './holders.errors';
 
 @Injectable()
 export class HoldersService implements IHoldersService {
-  create(data: ICreateHolderData): Promise<Holder> {
-    throw new Error('Method not implemented.');
+  constructor(private prismaService: PrismaService) {}
+
+  async create(data: ICreateHolderData): Promise<Holder> {
+    const alreadyRegistered = await this.prismaService.holder.findUnique({
+      where: {
+        document: data.document,
+      },
+    });
+
+    if (alreadyRegistered) {
+      throw holdersErrors.ALREADY_REGISTERED;
+    }
+
+    const holder = await this.prismaService.holder.create({
+      data,
+    });
+
+    return holder;
   }
 }
