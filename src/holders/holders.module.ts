bb3b51a79b178f65a5197d6b57596d80cd233fac
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-08 12:39:56 -0300

    add holders controller

--- src/holders/holders.module.ts
+++ src/holders/holders.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HoldersService } from './holders.service';
+import { HoldersController } from './holders.controller';
 
 @Module({
-  providers: [HoldersService]
+  providers: [HoldersService],
+  controllers: [HoldersController]
 })
 export class HoldersModule {}
