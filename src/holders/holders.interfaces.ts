3268f2ff0b65ea8d2b2801fff3ae506fdbeac16d
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-08 13:01:57 -0300

    add interfaces for create method on controller

--- src/holders/holders.interfaces.ts
+++ src/holders/holders.interfaces.ts
@@ -3,6 +3,10 @@ import { CreateHolderDto } from './dtos/create-holder.dto';
 
 export interface ICreateHolderData extends Omit<CreateHolderDto, 'password'> {}
 
+export interface IHoldersController {
+  create(data: CreateHolderDto): Promise<Holder>;
+}
+
 export interface IHoldersService {
   create(data: ICreateHolderData): Promise<Holder>;
   getById(id: string): Promise<Holder>;
