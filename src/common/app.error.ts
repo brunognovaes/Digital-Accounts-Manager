2675727212daface4d930e22ed019c65a26a93c3
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-07 21:08:51 -0300

    add app error

---
+++ src/common/app.error.ts
@@ -0,0 +1,13 @@
+import { HttpStatus } from '@nestjs/common';
+
+export enum ErrorCodes {}
+
+export class AppError extends Error {
+  constructor(
+    public message: string,
+    public httpStatus: HttpStatus,
+    public errorCode: ErrorCodes,
+  ) {
+    super(message);
+  }
+}
