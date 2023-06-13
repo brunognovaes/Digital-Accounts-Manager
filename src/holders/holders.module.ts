56305d4eca72d640beb6747cc114779fa8f5bf71
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-13 19:21:08 -0300

    update module dependency

--- src/holders/holders.module.ts
+++ src/holders/holders.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AccountsService } from 'src/accounts/accounts.service';
 import { AuthService } from 'src/auth/auth.service';
 import { PrismaService } from 'src/prisma.service';
 import { HoldersController } from './holders.controller';
 import { HoldersService } from './holders.service';
 
 @Module({
-  providers: [HoldersService, PrismaService, AuthService],
+  providers: [HoldersService, PrismaService, AuthService, AccountsService],
   controllers: [HoldersController],
   exports: [HoldersService],
 })
