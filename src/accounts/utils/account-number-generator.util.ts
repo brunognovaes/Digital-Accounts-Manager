55cc6831a469bf4b82195c4bf579f1e8e86e091c
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-12 22:04:01 -0300

    add accounts utils for services

---
+++ src/accounts/utils/account-number-generator.util.ts
@@ -0,0 +1,15 @@
+export const accountNumberGenerator = (length: number) => {
+  const accountPrefix = '1971'
+  const minLength = 6
+  if (length < minLength) {
+    throw new Error(`Cannot generate an account number with less than ${minLength} of length.`)
+  }
+
+  let accountNumber = accountPrefix;
+
+  for (let i = 0; i < (length = accountPrefix.length); i += 1) {
+    accountNumber += String(Math.floor(Math.random() * 10));
+  }
+
+  return accountNumber
+}
