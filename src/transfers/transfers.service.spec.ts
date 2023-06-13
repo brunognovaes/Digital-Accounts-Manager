ff2e5399a935285c31cf6f18cb39534dec36f331
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-12 22:50:14 -0300

    add transfers service and test

---
+++ src/transfers/transfers.service.spec.ts
@@ -0,0 +1,139 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Prisma, Transfer, TransferStatus } from '@prisma/client';
+import { AppError } from 'src/common/error/app.error';
+import { IPaginatedResponse } from 'src/common/index.interfaces';
+import { PrismaService } from 'src/prisma.service';
+import transfersErrors from './transfers.errors';
+import { TransfersService } from './transfers.service';
+
+const mockId = "id"
+const mockAccountId = "accountId"
+const mockAmount = 10
+const mockMessage = "message"
+const mockStatus = TransferStatus.PENDING
+
+const mockTransfer: Transfer = {
+  id: mockId,
+  account_id: mockAccountId,
+  amount: new Prisma.Decimal(mockAmount),
+  credit: true,
+  message: mockMessage,
+  status: mockStatus,
+  created_at: new Date('01-01-01T00:00:00Z'),
+  updated_at: new Date('01-01-01T00:00:00Z'),
+}
+
+const mockPaginatedResponse: IPaginatedResponse<Transfer> = {
+  metadata: {
+    currentItems: 2,
+    maxPage: 0,
+    order: Prisma.SortOrder.desc,
+    page: 0
+  },
+  values: [mockTransfer, mockTransfer]
+}
+
+const mockPrisma = {
+  transfer: {
+    create: async () => mockTransfer,
+    findUnique: async () => mockTransfer,
+    findMany: async () => [mockTransfer, mockTransfer],
+    update: async () => mockTransfer,
+    count: async () => 2
+  }
+}
+
+describe('TransfersService', () => {
+  let transfersService: TransfersService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [TransfersService, {
+        provide: PrismaService,
+        useValue: mockPrisma
+      }],
+    }).compile();
+
+    transfersService = module.get<TransfersService>(TransfersService);
+
+    jest.resetAllMocks()
+  });
+
+  it('should be defined', () => {
+    expect(transfersService).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create a transfer successfully', () => {
+       const response = transfersService.create({
+        amount: mockAmount,
+        credit: true,
+        message: mockMessage
+       }, mockAccountId)
+ 
+       expect(response).resolves.toBeDefined()
+       expect(response).resolves.toEqual(mockTransfer)
+     })
+  })
+
+  describe('getById', () => {
+    it('should return a account successfully', () => {
+      const response = transfersService.getById(mockId);
+
+      expect(response).resolves.toBeDefined();
+      expect(response).resolves.toEqual(mockTransfer);
+    });
+
+    it('should throw an error when holder not found', () => {
+      jest.spyOn(mockPrisma.transfer, 'findUnique').mockResolvedValue(null);
+
+      const response = transfersService.getById(mockId);
+
+      expect(response).rejects.toBeDefined();
+      expect(response).rejects.toThrowError(AppError);
+      expect(response).rejects.toEqual(transfersErrors.NOT_FOUND);
+    });
+  })
+
+  describe('list', () => {
+    it('should return a list of accounts and metadatas', () => {
+      const response = transfersService.list({
+        itemsPerPage: 2,
+        order: Prisma.SortOrder.desc,
+        page: 0
+      })
+
+      expect(response).resolves.toBeDefined()
+      expect(response).resolves.toEqual(mockPaginatedResponse)
+    })
+  })
+
+  describe('processStatus', () => {
+    it('should update status successfully when transfer status is PENDING', () => {
+      const response = transfersService.processStatus(mockId, TransferStatus.APPROVED)
+
+      expect(response).resolves.toBeDefined()
+      expect(response).resolves.toEqual(mockTransfer)
+    })
+
+    it('should throw an error when transfer status is APPROVED', () => {
+      jest.spyOn(mockPrisma.transfer, 'findUnique').mockResolvedValue({...mockTransfer, status: TransferStatus.APPROVED});
+
+      const response = transfersService.processStatus(mockId, TransferStatus.APPROVED)
+
+      expect(response).rejects.toBeDefined();
+      expect(response).rejects.toThrowError(AppError);
+      expect(response).rejects.toEqual(transfersErrors.ALREADY_PROCESSED);
+    })
+
+    it('should throw an error when transfer status is REFUSED', () => {
+      jest.spyOn(mockPrisma.transfer, 'findUnique').mockResolvedValue({...mockTransfer, status: TransferStatus.REFUSED});
+
+      const response = transfersService.processStatus(mockId, TransferStatus.APPROVED)
+
+      expect(response).rejects.toBeDefined();
+      expect(response).rejects.toThrowError(AppError);
+      expect(response).rejects.toEqual(transfersErrors.ALREADY_PROCESSED);
+    })
+  })
+});
---
+++ src/transfers/transfers.service.ts
@@ -0,0 +1,92 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { Transfer, TransferStatus } from '@prisma/client';
+import { IPaginatedResponse } from 'src/common/index.interfaces';
+import { PrismaService } from 'src/prisma.service';
+import { CreateTransferDto } from './dtos/create-transfer.dto';
+import transfersErrors from './transfers.errors';
+import { IListTransfersFilterQuery, ITransfersService } from './transfers.interfaces';
+
+@Injectable()
+export class TransfersService implements ITransfersService {
+  constructor(@Inject(PrismaService) private prismaService: PrismaService) {}
+
+  create(data: CreateTransferDto, accountId: string): Promise<Transfer> {
+    return this.prismaService.transfer.create({
+      data: {
+        amount: data.amount,
+        credit: data.credit,
+        message: data.message,
+        status: TransferStatus.PENDING,
+        account_id: accountId,
+      }
+    })
+  }
+
+  async getById(id: string): Promise<Transfer> {
+    const transfer = await this.prismaService.transfer.findUnique({
+      where: {
+        id
+      }
+    })
+
+    if (!transfer) {
+      throw transfersErrors.NOT_FOUND
+    }
+
+    return transfer
+  }
+
+  async list(queries: IListTransfersFilterQuery): Promise<IPaginatedResponse<Transfer>> {
+    const orderBy = {
+      created_at: queries.order,
+    };
+    const skip = queries.itemsPerPage * queries.page;
+    const take = queries.itemsPerPage;
+    const gte = new Date(queries.startDate)
+    const lte = new Date(new Date(queries.endDate).setUTCHours(23, 59, 59, 59))
+    const where = {
+      created_at: {
+        ...(queries.startDate && { gte }),
+        ...(queries.endDate && { lte }),
+        },
+      ...(queries.accountId && { account_id: queries.accountId })
+    }
+
+    const maxItems = await this.prismaService.transfer.count({
+      where
+    });
+    const maxPage = Math.floor(maxItems / queries.itemsPerPage) - 1;
+
+    const transfers = await this.prismaService.transfer.findMany({
+      where,
+      skip,
+      orderBy,
+      take,
+    });
+
+    return { values: transfers, metadata: {
+      currentItems: transfers.length,
+      maxPage,
+      order: queries.order,
+      page: queries.page,
+    } 
+  }
+  }
+
+  async processStatus(id: string, status: TransferStatus): Promise<Transfer> {
+    const transfer = await this.getById(id)
+
+    if (transfer.status !== TransferStatus.PENDING) {
+      throw transfersErrors.ALREADY_PROCESSED
+    }
+
+    return this.prismaService.transfer.update({
+      where: {
+        id: id,
+      },
+      data: {
+        status
+      }
+    })
+  }
+}
