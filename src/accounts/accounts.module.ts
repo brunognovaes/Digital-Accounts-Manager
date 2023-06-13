f2da59afc01324dcb92f290d78014d2822f86554
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-13 17:59:35 -0300

    fix modules dependecies

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HoldersModule } from 'src/holders/holders.module';
 import { PrismaService } from 'src/prisma.service';
-import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
+import { AccountsService } from './accounts.service';
 
 @Module({
   providers: [AccountsService, PrismaService],
-  exports: [AccountsService],
   controllers: [AccountsController],
+  imports: [HoldersModule],
+  exports: [AccountsService],
 })
 export class AccountsModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AccountsModule } from './accounts/accounts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { HoldersModule } from './holders/holders.module';
-import { AccountsModule } from './accounts/accounts.module';
 import { TransfersModule } from './transfers/transfers.module';
 
 @Module({
-  imports: [AuthModule, HoldersModule, AccountsModule, TransfersModule],
+  imports: [
+    AuthModule,
+    HoldersModule,
+    AccountsModule,
+    TransfersModule,
+    JwtModule.register({
+      global: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
