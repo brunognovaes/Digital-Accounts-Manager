6c7d003914eb91666fe2f0e89e6e89eece234878
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-13 00:42:43 -0300

    update module dependencies

--- src/transfers/transfers.module.ts
+++ src/transfers/transfers.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { AccountsModule } from 'src/accounts/accounts.module';
+import { PrismaService } from 'src/prisma.service';
+import { TransfersController } from './transfers.controller';
 import { TransfersService } from './transfers.service';
 
 @Module({
-  providers: [TransfersService]
+  providers: [TransfersService, PrismaService],
+  controllers: [TransfersController],
+  imports: [AccountsModule]
 })
 export class TransfersModule {}
