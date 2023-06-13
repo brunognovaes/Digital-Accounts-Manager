2a39253776a2a0c075ef39e8b7fcd79603278b20
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-12 22:03:22 -0300

    add accounts module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { HoldersModule } from './holders/holders.module';
+import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
-  imports: [AuthModule, HoldersModule],
+  imports: [AuthModule, HoldersModule, AccountsModule],
   controllers: [AppController],
   providers: [AppService],
 })
