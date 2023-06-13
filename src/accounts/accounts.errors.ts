2970b40de4799aa627d281588444aae3a50aeca8
Bruno Gomes <brunognovaes.cm@gmail.com>
2023-06-12 22:03:51 -0300

    add accounts error

---
+++ src/accounts/accounts.errors.ts
@@ -0,0 +1,9 @@
+import { HttpStatus } from "@nestjs/common";
+import { AppError, ErrorCodes } from "src/common/error/app.error";
+
+export default {
+  NOT_FOUND: new AppError("Account not found.", HttpStatus.NOT_FOUND, ErrorCodes.NOT_FOUND),
+  INACTIVE: new AppError("Account is inactive.", HttpStatus.FORBIDDEN, ErrorCodes.INACTIVE_ACCOUNT),
+  BLOCKED: new AppError("Account is blocked.", HttpStatus.FORBIDDEN, ErrorCodes.BLOCKED_ACCOUNT),
+  NOT_ENOUGH_BALANCE: new AppError("Not enough balance.", HttpStatus.UNPROCESSABLE_ENTITY, ErrorCodes.INSUFFICIENT_BALANCE)
+}
